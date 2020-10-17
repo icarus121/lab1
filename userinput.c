41c18a1f7ad319304f8b5e0148ae99bc72b10768
atikah <2020974753@isiswa.uitm.edu.my>
2020-10-17 14:19:10 +0800

    my second commit

---
+++ userinput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+/*This is y second program*/
+int age;
+printf("Hi, how old are you?");
+scanf("%d", &age);
+printf("You are %d years old", age);
+return(0);
+}
